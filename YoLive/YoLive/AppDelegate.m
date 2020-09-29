fa5f70941d5a4604364fcc1961b3eaecc7bc6d4e
zhouyu521 <176262251@qq.com>
2020-09-29 15:33:02 +0800

    1
    1

--- YoLive/YoLive/AppDelegate.m
+++ YoLive/YoLive/AppDelegate.m
@@ -23,6 +23,27 @@
         432
         '4
         234
+        sdfds
+        struct __STDC_UTF_16__sd
+        NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+        NSEntityDescription *entity = [NSEntityDescription entityForName:@"sdf
+                                       __STDC_UTF_16__" inManagedObjectContext:<#context#>];
+        [fetchRequest setEntity:entity];
+        // Specify criteria for filtering which objects to fetch
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"<#format string#>", <#arguments#>];
+        [fetchRequest setPredicate:predicate];
+        // Specify how the fetched objects should be sorted
+        NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"<#key#>"
+                                                                       ascending:YES];
+        [fetchRequest setSortDescriptors:[NSArray arrayWithObjects:sortDescriptor, nil]];
+        
+        NSError *error = nil;
+        NSArray *fetchedObjects = [<#context#> executeFetchRequest:fetchRequest error:&error];
+        if (fetchedObjects == nil) {
+            <#Error handling code#>
+        }{
+            <#struct fields#>
+        };
     });
     return YES;
 }
